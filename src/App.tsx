96ff410f3abddbde3e2f7432a3def55f7d47f307
jaciane1990 <jaciane.izidro@gmail.com>
2025-08-22 11:28:55 -0300

    Commit estilização da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,39 @@ function App() {
     setLoading(true);
     setMessage("");
 
-    // Simula uma requisição (ex: login, salvar dados)
     setTimeout(() => {
       setLoading(false);
       setMessage("Ação concluída com sucesso!");
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     }, 2000);
   }
 
   return (
-    <div>
-      <h1>Exercício: Botão de Carregamento</h1>
-      <br />
-      <p>Esse código simula uma operação de envio de dados que demora 2 segundos.</p>
-      <button
-        className="btn"
-        onClick={handleClick}
-        disabled={loading} // desabilita o botão ao carregar
-      >
-        {loading ? (
-          <>
-            <span className="spinner" aria-hidden="true"></span> Enviando...
-          </>
-        ) : (
-          "Enviar"
-        )}
-      </button>
-      <br />
-      {message && <p className="success">{message}</p>}
-      
-    </div>
+    <>
+      <div className="container">
+        <h1>Exercício: Botão de Carregamento</h1>
+        <p>Esse código simula uma operação de envio de dados que demora 2 segundos.</p>
+        <button
+          className="btn"
+          onClick={handleClick}
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              <span className="spinner" aria-hidden="true"></span>
+              Enviando...
+            </>
+          ) : (
+            "Enviar"
+          )}
+        </button>
+      </div>
+
+      {message && <div className="toast">{message}</div>}
+    </>
   );
 }
 
